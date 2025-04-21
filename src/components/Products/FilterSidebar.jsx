3e604a98a6cd26ef3c1ec4b38c52c8498a22aa0c
Richard <lcdo.richardflores@gmail.com>
2025-04-20 21:56:38 -0400

    completing filter sidebar

--- src/components/Products/FilterSidebar.jsx
+++ src/components/Products/FilterSidebar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { TbBackground } from "react-icons/tb";
 import { useSearchParams } from "react-router-dom";
 
 const FilterSidebar = () => {
@@ -38,6 +39,22 @@ const FilterSidebar = () => {
 		setPriceRange([0, params.maxPrice || 100]);
 	}, [searchParams]);
 
+	const handleFilterChange = (e) => {
+		const { name, value, checked, type } = e.target;
+		let newFilters = { ...filters };
+		if (type === "checkbox") {
+			if (checked) {
+				newFilters[name] = [...(newFilters[name] || []), value];
+			} else {
+				newFilters[name] = newFilters[name].filter((item) => item !== value);
+			}
+		} else {
+			newFilters[name] = value;
+		}
+		setFilters(newFilters);
+		console.log(newFilters);
+	};
+
 	return (
 		<div className="p-4">
 			<h3 className="text-xl font-medium text-gray-800 mb-4">Filter</h3>
@@ -48,6 +65,8 @@ const FilterSidebar = () => {
 						<input
 							type="radio"
 							name="category"
+							value={category}
+							onChange={handleFilterChange}
 							className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
 						/>
 						<span className="text-gray-700">{category}</span>
@@ -61,11 +80,91 @@ const FilterSidebar = () => {
 						<input
 							type="radio"
 							name="gender"
+							value={gender}
+							onChange={handleFilterChange}
 							className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
 						/>
 						<span className="text-gray-700">{gender}</span>
 					</div>
 				))}
+			</div>
+			<div className="mb-6">
+				<label className="block text-gray-600 font-medium mb-2">Color</label>
+				<div className="flex flex-wrap gap-2">
+					{colors.map((color) => (
+						<button
+							key={color}
+							name="color"
+							value={color}
+							onChange={handleFilterChange}
+							className="w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105"
+							style={{ backgroundColor: color.toLowerCase() }}></button>
+					))}
+				</div>
+			</div>
+			<div className="mb-6">
+				<label className="block text-gray-600 font-medium mb-2">Size</label>
+				{sizes.map((size) => (
+					<div key={size} className="flex items-center mb-2">
+						<input
+							type="checkbox"
+							name="size"
+							value={size}
+							onChange={handleFilterChange}
+							className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+						/>
+						<span className="text-gray-700">{size}</span>
+					</div>
+				))}
+			</div>
+			<div className="mb-6">
+				<label className="block text-gray-600 font-medium mb-2">Material</label>
+				{materials.map((material) => (
+					<div key={material} className="flex items-center mb-2">
+						<input
+							type="checkbox"
+							name="material"
+							value={material}
+							onChange={handleFilterChange}
+							className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+						/>
+						<span className="text-gray-700">{material}</span>
+					</div>
+				))}
+			</div>
+			<div className="mb-6">
+				<label className="block text-gray-600 font-medium mb-2">Brands</label>
+				{brands.map((brand) => (
+					<div key={brand} className="flex items-center mb-2">
+						<input
+							type="checkbox"
+							name="brand"
+							value={brand}
+							onChange={handleFilterChange}
+							className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+						/>
+						<span className="text-gray-700">{brand}</span>
+					</div>
+				))}
+			</div>
+
+			<div className="mb-8">
+				<label className="block text-gray-600 font-medium mb-2 ">
+					Price Range
+				</label>
+				<input
+					type="range"
+					name="priceRange"
+					value={priceRange[0]}
+					onChange={(e) => setPriceRange([e.target.value, priceRange[1]])}
+					min={0}
+					max={100}
+					className="w-full h-2 bg-gray-300 appearance-none cursor-pointer"
+				/>
+				<div className="flex justify-between text-gray-600 mt-2">
+					<span>$0</span>
+					<span>${priceRange[1]}</span>
+				</div>
 			</div>
 		</div>
 	);
