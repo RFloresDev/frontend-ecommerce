9a19feb2cfaba2b954692c9a30c0f5f3de7785e6
rfloresdev <lcdo.richardflores@gmail.com>
2025-06-03 18:26:11 -0400

    little changes on filters

--- src/components/Products/SortOptions.jsx
+++ src/components/Products/SortOptions.jsx
@@ -1,5 +1,14 @@
 const SortOptions = () => {
-	return <div>SortOptions</div>;
+  return (
+    <div className="mb-4 flex items-center justify-end">
+      <select id="sort" className="border p-2 rounded-md focus:outline-none">
+        <option value="">Default</option>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+    </div>
+  );
 };
 
 export default SortOptions;
